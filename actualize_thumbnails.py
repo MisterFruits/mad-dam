ef8977142d811ab483fa5ac280ef0ea397fcd74f
Victor <vic.toad.tor@free.fr>
2015-09-06 15:16:30 +0200

    IMPROVE exclude not images files

--- actualize_thumbnails.py
+++ actualize_thumbnails.py
@@ -8,11 +8,16 @@ import argparse
 DEFAULT_SIZE = (256, 256)
 
 
-def maj_thumbnail(large_dir, thumbnail_dir):
-    for original_image in os.listdir(large_dir):
-        im = Image.open(os.path.join(large_dir, original_image))
-        im.thumbnail(DEFAULT_SIZE, Image.ANTIALIAS)
-        im.save(os.path.join(thumbnail_dir, original_image))
+def maj_thumbnails(large_dir, thumbnail_dir, size=DEFAULT_SIZE):
+    for img, name in iterimages(large_dir):
+        img.thumbnail(DEFAULT_SIZE, Image.ANTIALIAS)
+        img.save(os.path.join(thumbnail_dir, name))
+
+
+def iterimages(idir, ext=('.jpg', '.png', '.bmp')):
+    for image in os.listdiar(idir):
+        if os.path.splitext(image) in ext:
+            yield Image.open(os.path.join(idir, image)), image
 
 
 def main():
@@ -22,7 +27,7 @@ of a bunch of images''')
     parser.add_argument("odir")
 
     args = parser.parse_args()
-    maj_thumbnail(args.idir, args.odir)
+    maj_thumbnails(args.idir, args.odir)
 
 if __name__ == '__main__':
     main()
